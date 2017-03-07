c4c82b6324076cac6776000bbd2b9409f259482e
augustjanse <augustj@kth.se>
2017-03-07 14:33:29 +0100

    Add LuaSocket snippet

--- input.lua
+++ input.lua
@@ -12,6 +12,34 @@
 --Function skeleton from http://tasvideos.org/LuaScripting.html
 
 --Declarations here
+
+--Snippet copied from http://w3.impa.br/~diego/software/luasocket/introduction.html#tcp
+function openConnection()
+ -- load namespace
+ local socket = require("socket")
+ -- create a TCP socket and bind it to the local host, at any port
+ local server = assert(socket.bind("*", 0))
+ -- find out which port the OS chose for us
+ local ip, port = server:getsockname()
+ -- print a message informing what's up
+ print("Please telnet to localhost on port " .. port)
+ print("After connecting, you have 10s to enter a line to be echoed")
+ -- loop forever waiting for clients
+ while 1 do
+   -- wait for a connection from any client
+   local client = server:accept()
+   -- make sure we don't block waiting for this client's line
+   client:settimeout(10)
+   -- receive the line
+   local line, err = client:receive()
+   -- if there was no error, send it back to the client
+   if not err then client:send(line .. "\n") end
+   -- done with client, close the object
+   client:close()
+ end
+end
+openConnection()
+
 function fn() -- Whenever there is an update to the display (which is supposed to be every frame but is also called in some emulators during when the display is paused)
  socket = require("socket")
  print(socket._VERSION)
