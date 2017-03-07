f11699011aaa0494cd65a66293df4718f4ff4975
augustjanse <augustj@kth.se>
2017-03-07 13:21:34 +0100

    Add import of "socket" with comment

--- input.lua
+++ input.lua
@@ -1,5 +1,9 @@
 --Script to run with an emulator with support for Lua scripting, mainly VBA-rr.
 
+--Requires LuaSocket which wasn't too easy to access from VBA-rr. What worked for me was downloading the directory to C:\Lua and setting environment variables (in Windows):
+--LUA_CPATH = C:\Lua\?.dll;?.dll
+--LUA_PATH = C:\Lua\lua\?.lua;?.lua
+
 --Resources:
 --Info: http://tasvideos.org/EmulatorResources/VBA/LuaScriptingFunctions.html
 --Emulator download: http://adelikat.tasvideos.org/emulatordownloads/vba-rerecording/vba-v24m-svn-r480.7z
@@ -9,6 +13,7 @@
 
 --Declarations here
 function fn() -- Whenever there is an update to the display (which is supposed to be every frame but is also called in some emulators during when the display is paused)
- --Statements
+ socket = require("socket")
+ print(socket._VERSION)
 end
 gui.register(fn)
