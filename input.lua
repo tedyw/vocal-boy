46633fc957f1983e878182c86534401c5c1bf865
augustjanse <augustj@kth.se>
2017-03-07 14:46:39 +0100

    Refactor echo server to work inside of fn()

--- input.lua
+++ input.lua
@@ -13,35 +13,32 @@
 
 --Declarations here
 
+--Opens a connection to 127.0.0.1:50414 and returns the client object.
 --Snippet modified from http://w3.impa.br/~diego/software/luasocket/introduction.html#tcp
 function openConnection()
  -- load namespace
- local socket = require("socket")
+ socket = require("socket")
  -- create a TCP socket and bind it to the local host, at any port
- local server = assert(socket.bind("127.0.0.1", 50414)) -- Local IP, arbitrary ephemeral port
+ server = assert(socket.bind("127.0.0.1", 50414)) -- Local IP, arbitrary ephemeral port
  -- find out which port the OS chose for us
  local ip, port = server:getsockname()
- -- print a message informing what's up
- print("Please telnet to localhost on port " .. port)
- print("After connecting, you have 10s to enter a line to be echoed")
- -- loop forever waiting for clients
- while 1 do
-   -- wait for a connection from any client
-   local client = server:accept()
-   -- make sure we don't block waiting for this client's line
-   client:settimeout(10)
-   -- receive the line
-   local line, err = client:receive()
-   -- if there was no error, send it back to the client
-   if not err then client:send(line .. "\n") end
-   -- done with client, close the object
-   client:close()
- end
+ -- wait for a connection from any client
+ local client = server:accept()
+
+ return client
 end
-openConnection()
+
+function closeConnection()
+ -- done with client, close the object
+ client:close()
+end
+
+client = openConnection()
 
 function fn() -- Whenever there is an update to the display (which is supposed to be every frame but is also called in some emulators during when the display is paused)
- socket = require("socket")
- print(socket._VERSION)
+ -- receive the line
+ local line, err = client:receive()
+ -- if there was no error, send it back to the client
+ if not err then client:send(line .. "\n") end
 end
 gui.register(fn)
